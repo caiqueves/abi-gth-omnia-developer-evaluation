bad50cb695795f207164fc5054737b67e6926874
Caíque Neves <caique.sire@gmail.com>
2024-11-29 04:42:26 -0300

    feat: create cascade no banco

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Migrations/20241129073759_AddCascadeDeleteToUserAndAddress.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteToUserAndAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Rating_RatingId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Long",
+                table: "Geolocation",
+                type: "numeric(9,6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "Geolocation",
+                type: "numeric(9,6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Rating_RatingId",
+                table: "Product",
+                column: "RatingId",
+                principalTable: "Rating",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Rating_RatingId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Long",
+                table: "Geolocation",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(9,6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lat",
+                table: "Geolocation",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(9,6)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Rating_RatingId",
+                table: "Product",
+                column: "RatingId",
+                principalTable: "Rating",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
